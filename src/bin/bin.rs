aa7a3bb5348f0c19e3b72016bc2ac7a8c4c8319b
Philipp <philipp@f1ndus.de>
2021-09-22 23:05:46 +0200

    rng stuff

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -4,6 +4,7 @@ extern crate serde_yaml;
 
 use serde_yaml::from_reader;
 use serde::{Serialize, Deserialize};
+use std::process::{Command, ExitStatus};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Sink {
@@ -43,6 +44,52 @@ struct Rule {
     rule: String
 }
 
+struct Manager<'a> {
+    ipt: iptables::IPTables,
+    config: &'a Config
+}
+
+impl Manager<'_> {
+
+    fn new(config: &Config) -> Manager {
+        Manager {
+            ipt: iptables::new(false).unwrap(),
+            config
+        }
+    }
+
+    fn add_iptables_rule(&self, portRule: &PortRule) {
+        self.ipt.append_unique("mangle", "PREROUTING", "");
+    }
+
+    fn test(&self) {
+       // self.ipt.exists()
+    }
+}
+
+struct IpRoute2 {
+
+}
+
+impl IpRoute2 {
+
+    fn get_cmd(&self) -> Command {
+        Command::new("ip")
+    }
+
+    fn add_route(&self, ip: &str, nic: &str, stdRoute: &str) {
+        let d = self.get_cmd().arg("route").arg("add").arg(ip).arg("via").arg(stdRoute).arg("dev").arg(nic).output().unwrap();
+        println!("{}", String::from_utf8(d.stderr).unwrap());
+        assert!(d.status.success());
+    }
+
+    fn delete_route(&self, ip: &str, nic: &str, stdRoute: &str) {
+        let d = self.get_cmd().arg("route").arg("delete").arg(ip).arg("via").arg(stdRoute).arg("dev").arg(nic).output().unwrap();
+        println!("{}", String::from_utf8(d.stderr).unwrap());
+        assert!(d.status.success());
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     let ipt = iptables::new(false).unwrap();
@@ -53,5 +100,12 @@ fn main() {
     let d: Config = from_reader(f).unwrap();
     println!("{:#?}", d);
 
+    let manager = Manager::new(&d);
+    manager.add_iptables_rule(&d.priority_ports.first().unwrap());
+    let d = IpRoute2{};
+    d.add_route("10.0.0.2", "eth0", "10.0.0.1")
+
 }
+
+
 
